3c63ef627bc2e9b6f77ede0319c735e4604254ec
goelaine <elainego@andrew.cmu.edu>
2024-04-20 23:32:06 -0400

    j

--- selecting_pathways_final.py
+++ selecting_pathways_final.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-# Load the CSV file into a pandas DataFrame
-df = pd.read_csv("compbio project csv manipulation core genes testing - Sheet1.csv", skiprows = [1,2])
+# Function to extract substring before '|'
+def extract_substring(text):
+    return text.split('|')[0]
+
+df = pd.read_csv("/Users/wanbo/Desktop/compbio project csv manipulation core genes testing - Sheet1.csv", skiprows = [1, 2])
 
 # Your dictionary of pathways (pathway to set of genes)
 pathway_genes = {
@@ -11,7 +14,7 @@ pathway_genes = {
 }
 
 keys = pathway_genes.keys()
-min = len(pathway_genes[keys[0]])
+min = 1000
 
 
 # Dictionary to store pathway-wise gene arrays for each column
@@ -38,10 +41,11 @@ for col_label, colum in df_excluded_first_column.items():
         # Iterate over each gene in the column
         for g in genes:
             # Check if the gene belongs to the pathway
-            for index, row in df.interrows():
-                if g == row[0]:
+            for index, row in df.iterrows():
+                if int(g) == row[0]:
                     P[col_label][path].append(row[col_label])
                     break
+        P[col_label][path].sort()
         
         # Store the gene array for this column and pathway
         # pathway_gene_arrays[pathway][column_name] = genes_in_column
